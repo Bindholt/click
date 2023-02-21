4b86fa789d5a268fa29b1dc26c62e45e67f14d2d
Bindholt <Cbindholt@gmail.com>
2023-02-21 01:36:36 +0100

    Added animation for hitting ball!

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ window.addEventListener("load", start);
 
 function start() {
   startAnimations();
-  document
-    .querySelector("#baseball_1_container")
-    .addEventListener("click", ballHit);
+  setEventListeners();
 }
 
 function startAnimations() {
@@ -13,6 +11,34 @@ function startAnimations() {
     .classList.add("pitcher1_straight");
 }
 
+function setEventListeners() {
+  document
+    .querySelector("#baseball_1_container")
+    .addEventListener("click", ballHit);
+}
+
 function ballHit() {
-  console.log("HOMERUN");
+  this.removeEventListener("click", ballHit);
+
+  console.log(this);
+
+  let transform = window.getComputedStyle(this).getPropertyValue("transform");
+
+  let values = transform.split("(")[1].split(")")[0].split(",");
+  //   debugger;
+  this.classList.remove("pitcher1_straight");
+  console.log(values);
+  this.style.transform =
+    "translate(" +
+    values[4] +
+    "px, " +
+    values[5] +
+    "px) scale(" +
+    values[0] +
+    ")";
+
+  this.classList.add("ball_hit");
+  console.log("pause");
+
+  //
 }
